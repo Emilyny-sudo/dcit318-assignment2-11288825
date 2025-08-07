1a3b64be28be378e8a06ea6387df39fbb521efee
MB 11in 2k15 <mb11in2k15@MBs-MacBook-Air.local>
2025-08-07 02:37:50 +0000

    Add Question 3: Interfaces - IMovable, Car, Bicycle

---
+++ Question3-Interfaces/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+interface IMovable
+{
+    void Move();
+}
+
+class Car : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("The car drives on the road.");
+    }
+}
+
+class Bicycle : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("The bicycle is pedaled on the path.");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IMovable car = new Car();
+        IMovable bicycle = new Bicycle();
+
+        car.Move();
+        bicycle.Move();
+    }
+}
